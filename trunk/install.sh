e2c2b8783a979d93b863e3ec862fbf54e7bc14f0
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2012-01-22 20:55:00 +0000

    Tagged for 0.5 release

---
+++ trunk/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+install -d -m 755 $DESTDIR/usr/bin
+install -d -m 755 $DESTDIR/usr/share/applications
+install -d -m 755 $DESTDIR/usr/share/gtkman
+install -m 755 src/gtkman $DESTDIR/usr/bin/
+install -m 644 src/gtkman.glade $DESTDIR/usr/share/gtkman/
+install -m 644 gtkman.desktop $DESTDIR/usr/share/applications/
+
+if [ -f man/gtkman.man ]; then
+	install -d -m 755 $DESTDIR/usr/man/man1
+	install -m 644 man/gtkman.man $DESTDIR/usr/man/man1/gtkman.1
+fi
+
+for i in `ls po/*.po|sed "s/po\/\(.*\)\.po/\1/"`; do
+	install -d -m 755 $DESTDIR/usr/share/locale/$i/LC_MESSAGES
+	install -m 644 po/$i.mo $DESTDIR/usr/share/locale/$i/LC_MESSAGES/gtkman.mo
+done
